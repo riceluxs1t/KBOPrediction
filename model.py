a28803fb072380469a11f50ba99856c76eb14601
Jay Ryu <bluejay9676@gmail.com>
2017-06-23 00:52:42 -0500

    Runner & Model update
    - Runner is now functional
    - Model has some minor fixes.

--- model.py
+++ model.py
@@ -16,15 +16,13 @@ from constants import *
 
 def selu(x):
     with ops.name_scope('elu') as scope:
-        alpha = 1.6732632423543772848170429916717
-        scale = 1.0507009873554804934193349852946
-        return scale*tf.where(x>=0.0, x, alpha*tf.nn.elu(x))
+        return SCALE * tf.where(x>=0.0, x, ALPHA * tf.nn.elu(x))
 
 
 def dropout_selu(
     x, 
     keep_prob, 
-    alpha= -1.7580993408473766, 
+    alpha= DROP_ALPHA, 
     fixedPointMean=0.0, 
     fixedPointVar=1.0, 
     noise_shape=None, 
@@ -68,58 +66,53 @@ def dropout_selu(
 
 class Model:
 
-    def __init__(self, sess, name):
+    def __init__(self, sess, name, learn_rate):
         self.sess = sess
         self.name = name
-        self._build_net()
+        self._build_net(learn_rate)
 
-    def _build_net(self):
+    def _build_net(self, learn_rate):
         with tf.variable_scope(self.name):
             # input place holders
-            self.X = tf.placeholder(tf.float32, [None, 40]) # TODO : Format data correctly.
+            self.X = tf.placeholder(tf.float32, [None, 58])
             self.Y = tf.placeholder(tf.float32, [None, 1]) # Win : 1 Lose : 0
 
             # dropout (keep_prob) rate  0.7 on training, but should be 1 for testing
             self.keep_prob = tf.placeholder(tf.float32)
 
             # weights & bias for nn layers
-            W1 = tf.get_variable("W1", shape=[40, 512],
+            W1 = tf.get_variable("W1", shape=[58, 100],
                                  initializer=tf.contrib.layers.xavier_initializer())
-            b1 = tf.Variable(tf.random_normal([512]))
-            L1 = selu(tf.matmul(X, W1) + b1)
-            L1 = dropout_selu(L1, keep_prob=keep_prob)
+            b1 = tf.Variable(tf.random_normal([100]))
+            L1 = selu(tf.matmul(self.X, W1) + b1)
+            L1 = dropout_selu(L1, keep_prob=self.keep_prob)
 
-            W2 = tf.get_variable("W2", shape=[512, 512],
+            W2 = tf.get_variable("W2", shape=[100, 100],
                                  initializer=tf.contrib.layers.xavier_initializer())
-            b2 = tf.Variable(tf.random_normal([512]))
+            b2 = tf.Variable(tf.random_normal([100]))
             L2 = selu(tf.matmul(L1, W2) + b2)
-            L2 = dropout_selu(L2, keep_prob=keep_prob)
+            L2 = dropout_selu(L2, keep_prob=self.keep_prob)
 
-            W3 = tf.get_variable("W3", shape=[512, 512],
+            W3 = tf.get_variable("W3", shape=[100, 50],
                                  initializer=tf.contrib.layers.xavier_initializer())
-            b3 = tf.Variable(tf.random_normal([512]))
+            b3 = tf.Variable(tf.random_normal([50]))
             L3 = selu(tf.matmul(L2, W3) + b3)
-            L3 = dropout_selu(L3, keep_prob=keep_prob)
+            L3 = dropout_selu(L3, keep_prob=self.keep_prob)
 
-            W4 = tf.get_variable("W4", shape=[512, 512],
+            W4 = tf.get_variable("W4", shape=[50, 1],
                                  initializer=tf.contrib.layers.xavier_initializer())
-            b4 = tf.Variable(tf.random_normal([512]))
-            L4 = selu(tf.matmul(L3, W4) + b4)
-            L4 = dropout_selu(L4, keep_prob=keep_prob)
-
-            W5 = tf.get_variable("W5", shape=[512, 1],
-                                 initializer=tf.contrib.layers.xavier_initializer())
-            b5 = tf.Variable(tf.random_normal([1]))
-            self.hypothesis = tf.matmul(L4, W5) + b5 # Probability of winning
+            b4 = tf.Variable(tf.random_normal([1]))
+            self.hypothesis = tf.matmul(L3, W4) + b4 # Probability of winning
 
         # define cost/loss & optimizer
-        self.cost = tf.reduce_sum(tf.square(self.hypothesis - Y)) # TODO: Need to find a more suiting convex function.
-        self.optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(cost)
+        # self.cost = tf.reduce_sum(tf.square(self.hypothesis - self.Y)) # TODO: Need to find a better cost func.
+        self.cost = tf.reduce_sum(tf.square(self.hypothesis - self.Y))
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=learn_rate).minimize(self.cost)
 
         # Test model and check accuracy
-        correct_prediction = tf.equal(
-            tf.argmax(self.hypothesis, 1), tf.argmax(self.Y, 1))
-        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        # predicted = tf.cast(self.hypothesis > 0.5, dtype=tf.float32)
+        # self.accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, self.Y), dtype=tf.float32))
+        self.accuracy = tf.reduce_mean(tf.abs(self.hypothesis - self.Y))
 
     def predict(self, x_test, keep_prop=1.0):
         return self.sess.run(self.hypothesis, feed_dict={self.X: x_test, self.keep_prob: keep_prop})
@@ -127,7 +120,7 @@ class Model:
     def get_accuracy(self, x_test, y_test, keep_prop=1.0):
         return self.sess.run(self.accuracy, feed_dict={self.X: x_test, self.Y: y_test, self.keep_prob: keep_prop})
 
-    def train(self, x_data, y_data, keep_prop=KEEP_RATE):
+    def train(self, x_data, y_data, keep_prop):
         return self.sess.run([self.cost, self.optimizer], feed_dict={
             self.X: x_data, self.Y: y_data, self.keep_prob: keep_prop})
     
@@ -140,13 +133,12 @@ class Runner:
     def __init__(self):
         tf.set_random_seed(777)  # reproducibility
 
-    def train_run(self, model, x_train, y_train):
+    def train_run(self, model, x_train, y_train, training_epoch, keep_prob):
         model.get_sess.run(tf.global_variables_initializer())
-        for epoch in xrange(TRAINING_EPOCHS):
-            feed_dict = {X: x_train, Y: y_train, keep_prob: KEEP_RATE}
-            c, _ = model.train([cost, optimizer], feed_dict=feed_dict)
-
-            print ('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(c))
+        for epoch in range(training_epoch):
+            c, _ = model.train(x_train, y_train, keep_prob)
+            if epoch % 100 == 0:
+            	print ('Epoch:', '%04d' % (epoch), 'cost =', '{:.9f}'.format(c))
 
         #TODO: Save frozen graph of the tf after training. 
         #Refer to: https://blog.metaflow.fr/tensorflow-how-to-freeze-a-model-and-serve-it-with-a-python-api-d4f3596b3adc
---
+++ trainer.py
@@ -0,0 +1,158 @@
+"""
+This is a runner for KBO Prediction.
+"""
+import os
+import sys
+import argparse
+import json
+import tensorflow as tf
+import numpy as np
+
+from elasticsearch import Elasticsearch
+
+from model import Model
+from model import Runner
+from constants import *
+
+
+DIRNAME = os.path.split(os.path.abspath(sys.argv[0]))[0]
+
+"""
+Formats the data into x_val and y_val
+@param JSON value
+@returns x_val, y_val
+"""
+def format(data):
+	home = []
+	away = []
+	# winner = [0] # Winner : 1 if home 0 if away
+	winner = [data['score_board']['summary']['home']['r']] # Home team's score
+
+	# Get the winner
+	# if data['score_board']['summary']['home']['r'] > data['score_board']['summary']['away']['r']:
+	# 	winner[0] = 1
+
+	for k1, v1 in data['score_board']['summary'].items():
+		if k1 == 'home':
+			for k2, v2 in v1.items():
+				if k2 != 'r':
+					home.append(v2)
+		else:
+			for k2, v2 in v1.items():
+				if k2 != 'r':
+					away.append(v2)
+
+	for k1, v1 in data['pitcher_info'].items():
+		if k1 == 'home':
+			for k2, v2 in v1[0].items():
+				if k2 != 'name':
+					if k2 == 'era':
+						home.append(float(v2))
+					else:	
+						home.append(v2)
+		else:
+			for k2, v2 in v1[0].items():
+				if k2 != 'name':
+					if k2 == 'era':
+						away.append(float(v2))
+					else:	
+						away.append(v2)
+
+	for k1, v1 in data['batter_info'].items():
+			if k1 == 'home':
+				for k2, v2 in v1[0].items():
+					if k2 != 'name':
+						if k2 == 'hra':
+							home.append(float(v2))
+						else:
+							home.append(v2)
+			else:
+				for k2, v2 in v1[0].items():
+					if k2 != 'name':
+						if k2 == 'hra':
+							away.append(float(v2))
+						else:
+							away.append(v2)
+
+	for k1, v1 in data['away_team_standing'].items():
+		if k1 != 'name':
+			away.append(v1)
+
+	for k1, v1 in data['home_team_standing'].items():
+		if k1 != 'name':
+			home.append(v1)
+
+	return home + away, winner
+
+
+# home, away, y = format(data[0])
+# print(len(home), len(away))
+# print(home)
+# print(away)
+# print(y)
+
+
+parser = argparse.ArgumentParser(description='KBO Score Prediction SELU NN')
+
+parser.add_argument('file_name', type=str, help='The data file (must be in the same directory')
+parser.add_argument('model_name', type=str, help='The name of the model')
+parser.add_argument('learn_rate', type=float, help='The learning rate')
+parser.add_argument('epoch', type=int, help='Training epoch')
+parser.add_argument('drop_rate', type=float, help='Drop rate')
+
+if __name__ == '__main__':
+	args = parser.parse_args()
+	"""
+	Create data set and divide into test data set & train data set
+	"""
+	f = open(DIRNAME + "/" + args.file_name, 'r')
+	print("Load JSON data")
+	data = json.load(f)
+
+	dataX = []
+	dataY = []
+
+	for i in range(len(data)):
+		# Put home team data
+		x_val, y_val = format(data[i])
+		dataX.append(x_val)
+		dataY.append(y_val)
+
+	# train_size = int(len(dataY) * 0.7)
+	# test_size = len(dataY) - train_size
+	# trainX, trainY = np.array(dataX[:train_size]), np.array(dataY[:train_size])
+	# testX, testY = np.array(dataX[train_size:]), np.array(dataY[train_size:])
+
+	## ======== Build model ======
+	with tf.Session() as sess:
+		kbo_pred_model = Model(
+			sess, 
+			args.model_name, 
+			learn_rate=args.learn_rate
+		)
+
+		## ======== Train model ======
+		kbo_runner = Runner()
+		# kbo_runner.train_run(kbo_pred_model, trainX, trainY, training_epoch=2000, keep_prob=0.7)
+		kbo_runner.train_run(
+			kbo_pred_model, 
+			dataX, 
+			dataY,
+			training_epoch=args.epoch, 
+			keep_prob=(1 - args.drop_rate)
+		)
+
+		#TODO Freeze the tf graph created here.
+
+	## ======== Run test =========
+	# accuracy = kbo_runner.get_accuracy(kbo_pred_model, testX, testY)
+
+	# print("Model Average Off Value: ")
+	# print(accuracy)
+
+
+
+
+
+
+
